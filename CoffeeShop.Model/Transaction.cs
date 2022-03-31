92ee00ef95504d2e2e9d41cf9acb3f120689fc4a
Kyriakos Mousias <kmousias1@gmail.com>
2022-03-31 16:07:22 +0300

    Ctor for transaction

--- CoffeeShop.Model/Transaction.cs
+++ CoffeeShop.Model/Transaction.cs
@@ -8,6 +8,10 @@ namespace CoffeeShop.Model
 {
     public class Transaction : BaseEntity
     {
+        public Transaction()
+        {
+
+        }
         public DateTime Date { get; set; }
         public int CustomerID { get; set; }
         public int EmployeeID { get; set; }
