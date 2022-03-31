ffd31e2fe86eee786a6be78cdc712cc7e5460626
rkeytech <rkeytech@protonmail.com>
2022-03-31 17:29:43 +0300

    Added CustomerRepo for EntityFramework

--- CoffeeShop.EF/Repositories/CustomerRepo.cs
+++ CoffeeShop.EF/Repositories/CustomerRepo.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CoffeeShop.EF.Context;
+using CoffeeShop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,46 @@ using System.Threading.Tasks;
 
 namespace CoffeeShop.EF.Repositories
 {
-    publci class CustomerRepo
+    public class CustomerRepo : IEntityRepo<Customer>
     {
+        public async Task Create(Customer entity)
+        {
+            using var context = new CoffeeShopContext();
+            context.Customers.Add(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            using var context = new CoffeeShopContext();
+            var foundCustomer = context.Customers.SingleOrDefault(customer => customer.Id == id);
+            if (foundCustomer != null)
+                return;
+            context.Customers.Remove(foundCustomer);
+            await context.SaveChangesAsync();
+        }
+
+        public List<Customer> GetAll()
+        {
+            using var context = new CoffeeShopContext();
+            return context.Customers.ToList();
+        }
+
+        public Customer? GetById(int id)
+        {
+            using var context = new CoffeeShopContext();
+            return context.Customers.Where(customer => customer.Id == id).SingleOrDefault();
+        }
+
+        public async Task Update(int id, Customer entity)
+        {
+            using var context = new CoffeeShopContext();
+            var foundCustomer = context.Customers.SingleOrDefault(customer => customer.Id == id);
+            if (foundCustomer is null)
+                return;
+            foundCustomer.Code = entity.Code;
+            foundCustomer.Description = entity.Description;
+            await context.SaveChangesAsync();
+        }
     }
 }
