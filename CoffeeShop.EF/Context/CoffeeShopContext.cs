a731bd2b66326af9fd88931e30ae2dd6b6750fb3
paoka <paoka@LAPTOP-1016MKVC>
2022-03-31 16:40:15 +0300

    added context

---
+++ CoffeeShop.EF/Context/CoffeeShopContext.cs
@@ -0,0 +1,35 @@
+﻿using CoffeeShop.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.EF.Context
+{
+    public class CoffeeShopContext : DbContext
+    {
+        public CoffeeShopContext()
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }  
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+    }
+}
