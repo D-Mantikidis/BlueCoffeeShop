97dfebdef2969031d32010ca02b442936ca83921
Dimitris Mantikidis <dimitrismad@hotmail.com>
2022-03-31 19:32:33 +0300

    Started MVC

--- CoffeeShop.Web/Program.cs
+++ CoffeeShop.Web/Program.cs
@@ -1,8 +1,10 @@
+using CoffeeShop.EF.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<CoffeeShopContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
