615722d1194e5b500dbfed1900ac32a858667e1b
paoka <paoka@LAPTOP-1016MKVC>
2022-04-01 18:05:06 +0300

    productviewmodel and controller

--- CoffeeShop.Web/Models/ProductViewModels.cs
+++ CoffeeShop.Web/Models/ProductViewModels.cs
@@ -1,6 +1,44 @@
-﻿namespace CoffeeShop.Web.Models
+﻿using CoffeeShop.Model;
+
+namespace CoffeeShop.Web.Models
 {
-    public class ProductViewModels
+    public class ProductListViewModel
     {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string ProductCategoryID { get; set; }
+        
+    }
+
+    public class ProductCreateViewModel
+    {
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string ProductCategoryID { get; set; }
+    }
+
+    public class ProductDeleteViewModel
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string ProductCategoryID { get; set; }
+    }
+
+    public class ProductUpdateViewModel
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string ProductCategoryID { get; set; }
     }
 }
