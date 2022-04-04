fc4a93d5903900d4e1e6ad4a626fd9bb979a3284
paoka <paoka@LAPTOP-1016MKVC>
2022-04-04 15:12:24 +0300

    hello world

---
+++ CoffeeShop.Model/Handlers/TransactionLineHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Model.Handlers
+{
+    public class TransactionLineHandler
+    {
+        public TransactionLineHandler()
+        {
+
+        }
+
+        public decimal GetTotalPrice(TransactionLine transactionLine)
+        {
+            return (transactionLine.Qty * transactionLine.Price)- transactionLine.Discount;
+        }
+    }
+}
